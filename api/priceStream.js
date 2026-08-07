8059c034311649e9f276fdc5ef8c57c4db9915cc
Samuel Babalola <babalolasamuel323@gmail.com>
2026-08-07 09:25:06 +0100

    fix

--- api/priceStream.js
+++ api/priceStream.js
@@ -1,14 +1,30 @@
-import { handleLivePrices } from "./routeHandlers/routeHandlers.js"
 
-export default async function handlePrice(req, res) {
 
-    await new Promise((resolve) => {
-        
-        handleLivePrices(res, req, 200);
-        
-        req.on('close', () => {
-            resolve(); 
-        })
-    })
+import { priceAlgo } from "../utils/priceAlgo.js";
 
+export default async function handler(req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  await new Promise((resolve) => {
+    
+    const interval = setInterval(() => {
+      const priceAlgor = priceAlgo();
+      res.write(
+        `data: ${JSON.stringify({
+          event: 'Price-Updated',
+          price: priceAlgor
+        })}\n\n`
+      );
+    }, 3000);
+
+    req.on('close', () => {
+      clearInterval(interval);
+      res.end();
+      resolve(); 
+    });
+    
+  });
 }
